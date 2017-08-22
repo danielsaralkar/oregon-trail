babb40ca44d7ad932536499049bb242cd1f7ebc9
Daniel Saralkar <danielsaralkar@gmail.com>
2017-08-22 11:40:56 +0300

    Added SQL Lite settings and migrated, Also Rendered login page

--- the_game/views.py
+++ the_game/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def home_page(request):
+    # return HttpResponse("You're looking at question")
+    return render(request, 'login.html', {"STATUS": "SUCCESS"})
