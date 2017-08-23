407eb0a46f48ec6e50db5534cb729f54ed839dbe
Daniel Saralkar <danielsaralkar@gmail.com>
2017-08-23 11:37:38 +0300

    Many changes / Added templates and login logout functions

--- the_game/urls.py
+++ the_game/urls.py
@@ -1,7 +1,14 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
+from django.contrib import admin
+# from django.contrib.auth.views import login, logout
+from django.contrib.auth import views as auth_views
+
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home_page, name='homepage'),
-
+    url(r'^$', views.home_page, name='home'),
+    url(r'^admin/', admin.site.urls),
+    url(r'^login/', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/login'}, name='logout'),
+    url(r'^register/', views.register, name="signup")
 ]
--- the_game/views.py
+++ the_game/views.py
@@ -1,8 +1,29 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
 
 # Create your views here.
 def home_page(request):
     # return HttpResponse("You're looking at question")
-    return render(request, 'login.html', {"STATUS": "SUCCESS"})
+    if request.user.is_authenticated():
+        return render(request, 'home.html', {"STATUS": "SUCCESS"})
+    else:
+        return HttpResponseRedirect("/login")
+
+
+# Register / Sign-Up to the website
+def register(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'register.html', {'form': form})
